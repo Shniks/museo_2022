87240e7d3b7278e2491fcc7f7584fe33cc9d7e82
Nikhil Shahi <shniks@gmail.com>
2022-06-30 11:32:11 -0700

    Created test and methods to check for existence and attributes. Completed iteration 1.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,17 @@
-require './lib/photograph'
+class Artist
+
+  attr_reader :id,
+              :name,
+              :born,
+              :died,
+              :country
+
+  def initialize(artist)
+    @id = artist[:id]
+    @name = artist[:name]
+    @born = artist[:born]
+    @died = artist[:died]
+    @country = artist[:country]
+  end
+
+end
