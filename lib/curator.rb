6d28683b1ae000c26604c0872ecb3e4f4b17d94b
Nikhil Shahi <shniks@gmail.com>
2022-06-30 11:37:47 -0700

    Created Curator and Curator Test classes.

---
+++ lib/curator.rb
@@ -0,0 +1,8 @@
+
+
+class Curator
+
+
+
+
+end 
---
+++ test/curator_test.rb
@@ -0,0 +1,19 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/curator'
+
+class CuratorTest < Minitest::Test
+
+  def test_if_it_exists
+    curator = Curator.new
+
+    assert_instance_of Curator, curator 
+  end
+
+
+
+
+end
